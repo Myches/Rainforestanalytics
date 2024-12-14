108a1afce06c0b6c004f78ded54604fa4fddc3d1
Michael Andorful <andorfulmichaelebo@gmail.com>
2024-12-14 16:19:57 +0000

    Add a toast

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,19 @@
-
+"Use client"
 import Overview from "../components/Overview"
-
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {useEffect} from "react"
 
 export default function DashboardPage(){
+
+    useEffect(() => {
+        toast.success("Welcome back to your dashboard!");
+    }, [])
     return (
 
         <div>
             <Overview  />
+            <ToastContainer />
         </div>
     )
 }
